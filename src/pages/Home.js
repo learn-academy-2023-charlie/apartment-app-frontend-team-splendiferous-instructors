3f00bbaf174f198889adba69eba2825d6bf8ffad
SunkissedQueen <baxtercm@outlook.com>
2023-06-14 00:14:59 -0700

    styled NotFound and Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 // import
 import React from "react"
+import homeImg from "../assets/homeImg.png"
+import { CardImg } from "reactstrap"
 // declare functional component
 const Home = () => {
   return(
     <>
-      <h3>Home page</h3>
+      <CardImg
+        alt="three tall cartoon apartment in a row, from left to right, blue, purple, and orange"
+        src={homeImg}
+        style={{
+          height: 500
+        }}
+        width="75%"
+      />
     </>
   )
 }
