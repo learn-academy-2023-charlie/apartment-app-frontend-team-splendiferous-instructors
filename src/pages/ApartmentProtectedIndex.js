3ac670272b9021e566cff62c16e0449728c1d98a
SunkissedQueen <baxtercm@outlook.com>
2023-06-14 13:50:50 -0700

    protected pages

--- src/pages/ApartmentProtectedIndex.js
+++ src/pages/ApartmentProtectedIndex.js
@@ -1,10 +1,36 @@
 // import
 import React from "react"
+import { Card, CardGroup, CardImg, CardBody, CardTitle, Button } from "reactstrap"
+
 // declare functional component
-const ApartmentProtectedIndex = () => {
+const ApartmentProtectedIndex = ({currentUser, apartments}) => {
+  
+  const myApartments = apartments?.filter(apartment => currentUser?.id === apartment.user_id)
+
   return(
     <>
-      <h3>ApartmentProtectedIndex page</h3>
+      <CardGroup>
+        {myApartments?.map((apt, index) => {
+          return(
+            <Card key={index}>
+              <CardImg
+                alt="a space you need to experience"
+                src={apt.image}
+                top
+                width="100%"
+              />
+              <CardBody>
+                <CardTitle tag="h5">
+                  Located in {apt.state}
+                </CardTitle>
+                <Button>
+                  More Details
+                </Button>
+              </CardBody>
+            </Card>
+          )
+        })}
+      </CardGroup>
     </>
   )
 }
